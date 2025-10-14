713eb8a13b12c1ee95c0c8b20ee67c5ea8d92059
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-15 08:32:40 +0900

    feat: 콘솔 입력 기능 추가
    - 사용자가 콘솔을 통해 입력값을 입력할 수 있는 기능 추가
    - try-catch문을 통해 입력이 올바르지 않은 경우 에러 탐지

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const USER_INPUT = await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+    } catch (error) {
+      MissionUtils.Console.print("[ERROR]");
+    }
+  }
 }
 
 export default App;
