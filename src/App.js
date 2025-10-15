79fdd01b06865dd4c633126b4cfbcd0ded065b9f
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-15 12:54:16 +0900

    feat: 입력값과 검증, 파싱 로직 연결

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { validator } from "./validator.js";
 
 class App {
   async run() {
@@ -6,6 +7,12 @@ class App {
       const USER_INPUT = await MissionUtils.Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요.\n"
       );
+      const [IS_INPUT_VALID, VALIDATION_RESULT] = validator(USER_INPUT);
+      if (IS_INPUT_VALID) {
+        Console.Print(calculate(VALIDATION_RESULT));
+        return;
+      }
+      Console.Print(VALIDATION_RESULT);
     } catch (error) {
       MissionUtils.Console.print("[ERROR]");
     }
