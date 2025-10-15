ffd8c4ff8ea0a760763001dfdccc9e38283d19cc
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-15 12:18:48 +0900

    refactor: 검증 단계 조건 수정
    - if문 중첩 방지
    - 조건 단순화

--- src/validator.js
+++ src/validator.js
@@ -2,13 +2,16 @@ import { REGEX, ERROR_MESSAGE } from "./constants.js";
 import { parser } from "./parser.js";
 
 export function validator(input) {
+  const PARSED_INPUT_ARRAY = parser(input);
+  const FILTERED_INPUT_ARRAY = PARSED_INPUT_ARRAY.filter(
+    (item) => typeof item === Number
+  );
+
   if (input.length === 0) return 0;
-  if (REGEX.test(input)) {
-    const PARSED_INPUT_ARRAY = parse(input);
-    const FILTERED_INPUT_ARRAY = PARSED_INPUT_ARRAY.filter(
-      (item) => typeof item === Number
-    );
-    if (PARSED_INPUT_ARRAY.length === FILTERED_INPUT_ARRAY.length) return true;
-  }
+  if (
+    PARSED_INPUT_ARRAY.length > 0 &&
+    PARSED_INPUT_ARRAY.length === FILTERED_INPUT_ARRAY.length
+  )
+    return true;
   return ERROR_MESSAGE;
 }
