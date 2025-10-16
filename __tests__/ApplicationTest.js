de18be9b43b5236e556e344fd582e04e9e72216b
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-16 14:21:46 +0900

    fix: 테스트케이스 제목 오타 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -89,7 +89,7 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트 : 입력 값 사이에 공백 1", async () => {
+  test("예외 테스트 : 입력 값 사이에 공백 2", async () => {
     const inputs = ["//h\\n4 ; 5 h2 ,1"];
     mockQuestions(inputs);
 
