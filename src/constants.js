39967c9cc8cf837f64b27ec1be1d94f9eb534754
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-16 14:59:24 +0900

    refactor: 커스텀 구분자 파싱 로직 수정
    - 기존 방법 삭제, 정규표현식 추가

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const DEFAULT_ERROR_MESSAGE = `[ERROR]`;
 export const BLANK_INPUT_ERROR_MESSAGE = `Error: blank input`;
 export const DEFAULT_DELIMITERS = `,;`;
+export const DELIMITER_REGEX = new RegExp("^//.\\\\n");
--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,18 @@
-import { DEFAULT_DELIMITERS } from "./constants.js";
+import {
+  DEFAULT_DELIMITERS,
+  DELIMITER_REGEX,
+  BLANK_DELIMITER_REGEX,
+} from "./constants.js";
 
 export function parser(input) {
   let DELIMITER = DEFAULT_DELIMITERS;
   let INPUT = input;
-  if (input.slice(0, 2) === "//" && input.slice(3, 5) === "\\n") {
+  if (DELIMITER_REGEX.test(INPUT)) {
     const NEW_DELIMITER = input.slice(2, 3);
     DELIMITER += NEW_DELIMITER;
     INPUT = input.slice(5);
   }
+
   const REGEX_FIND_NUMBERS = new RegExp(`[${DELIMITER}]`);
   return INPUT.split(REGEX_FIND_NUMBERS);
 }
