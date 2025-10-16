69754711cdd046ccbf8f65247f8372f8ebebb125
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-16 14:12:51 +0900

    fix: 테스트케이스 오류 수정
    - 입력 값에 커스텀 구분아 외에 다른 문자 테스트와 소수 값 입력 테스트에 이중 app.run() 제거

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -57,11 +57,10 @@ describe("문자열 계산기", () => {
   });
 
   test("예외 테스트 : 입력 값에 커스텀 구분자 외에 다른 문자", async () => {
-    const inputs = ["//d\n2d3,8g7"];
+    const inputs = ["//d\\n2d3,8g7"];
     mockQuestions(inputs);
 
     const app = new App();
-    await app.run();
 
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
@@ -71,7 +70,6 @@ describe("문자열 계산기", () => {
     mockQuestions(inputs);
 
     const app = new App();
-    await app.run();
 
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
