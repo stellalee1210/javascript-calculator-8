b03d09870bdbd5e0d29770266187e44c855e258b
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-16 14:22:51 +0900

    test: 커스텀 구분자에 기호 없는 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -103,4 +103,19 @@ describe("문자열 계산기", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+
+  test("예외 테스트 : 입력 값 사이에 공백 1", async () => {
+    const inputs = ["//\\n8;2,5"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["15"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
